857b8198d701c85862533f1e3de0b25d5f555f7b
tmalveirac <tmalveirac@gmail.com>
2013-10-08 18:03:34 -0300

    Exemplo Web Service

---
+++ src/webchat/WebChat.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package webchat;
+
+/**
+ *
+ * @author malveira
+ */
+public class WebChat {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
